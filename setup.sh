59c0031742a67b74019994220ab55c542b370819
Colin Murphy <cmurphy1321@gmail.com>
2014-12-27 00:00:39 -0500

    Make setup.sh back up only existing files.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 dir=$HOME/dotfiles
 backup=$HOME/.dotfiles.bak
-
-
+home_dir=$HOME/
 
 mkdir -p $backup
+
+for file in $home_dir/.*
+do
+    filename=$(basename $file)
+    if [[ "$filename" != "$(basename $0)" ]]; then
+        if [[ "$filename" = ".inputrc" || "$filename" = ".vimrc" || "$filename" = ".bashrc" || "$filename" = ".vim" ]]; then
+            echo "Backing up .$filename"
+            mv $HOME/$filename $backup/
+        fi
+    fi
+done
+
 for file in $dir/*
 do
     filename=$(basename $file)
     if [[ "$filename" != "$(basename $0)" ]]; then
         if [[ "$filename" = "inputrc" || "$filename" = "vimrc" || "$filename" = "bashrc" || "$filename" = "vim" ]]; then
             echo "Creating link for .$filename"
-            # Move existing dotfile to $backup
-            mv $HOME/.$filename $backup/
-            # Create symlink
             ln -s $dir/$filename $HOME/.$filename
         fi
     fi
 done
 
-# Pull in # submodules
+# Pull in submodules
 git submodule init && git submodule update
