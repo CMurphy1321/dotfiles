14613a2f9af954da4db6c56fa04213597f17d20b
Colin Murphy <colinthemurphy@gmail.com>
2015-01-09 20:33:02 -0500

    Change setup file, add gitignore.

--- setup.sh
+++ setup.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 dir=$HOME/dotfiles
-backup=$HOME/.dotfiles.bak
 home_dir=$HOME/
 
-mkdir -p $backup
+# Remove current instances of config files
+rm -rf $HOME/.vim
+rm -f $HOME/.vimrc
+rm -f $HOME/.bashrc
+rm -f $HOME/.inputrc
+rm -f $HOME/.ssh/config
+rm -f $HOME/.ssh/authorized_keys
 
-for file in $home_dir/.*
-do
-    filename=$(basename $file)
-    if [[ "$filename" != "$(basename $0)" ]]; then
-        if [[ "$filename" = ".inputrc" || "$filename" = ".vimrc" || "$filename" = ".bashrc" || "$filename" = ".ssh" ]]; then
-            echo "Backing up .$filename"
-            mv $HOME/$filename $backup/
-        fi
-    fi
-done
-
-for file in $dir/*
-do
-    filename=$(basename $file)
-    if [[ "$filename" != "$(basename $0)" ]]; then
-        if [[ "$filename" = "inputrc" || "$filename" = "vimrc" || "$filename" = "bashrc"  || "$filename" = "ssh" ]]; then
-            echo "Creating link for .$filename"
-            ln -s $dir/$filename $HOME/.$filename
-        fi
-    fi
-done
-
-if [ -f /home/jester/dotfiles/README.md ]; then
-    mv $HOME/.vim $backup/
-fi
+# Link config files
+mkdir -p $HOME/.ssh/
 ln -s $dir/vim $HOME/.vim
+ln -s $dir/ssh/config $HOME/.ssh/config
+ln -s $dir/ssh/authorized_keys $HOME/.ssh/authorized_keys
+ln -s $dir/vimrc $HOME/.vimrc
+ln -s $dir/bashrc $HOME/.bashrc
+ln -s $dir/inputrc $HOME/.inputrc
+
 # Pull in submodules
 git submodule init && git submodule update
 git submodule update --init --recursive
+
+# Compile YCM
 mkdir -p $HOME/ycmbuild && cd $HOME/ycmbuild
 cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp || pass="failed"
 if [[ "$pass" = "failed" ]]; then
@@ -41,9 +31,18 @@ if [[ "$pass" = "failed" ]]; then
     exit
 fi
 make ycm_support_libs -j4
-rm -rf ~/ycmbuild
+rm -rf $HOME/ycmbuild
+
+# Make ssh key if there isn't one
+if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa || pass="failed"
+    cat ~/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+else
+    echo "Existing ssh keys detected. Make sure yours is in the authorized_keys folder to ensure easy sshing!"
+fi
+
+# Check to make sure ssh-keygen ran, not applicable most of the time
 if [[ "$pass" = "failed" ]]; then
     echo "ssh-keygen failed! Please make sure you have ssh installed."
     exit
 fi
-cat ~/.ssh/id_rsa.pub >> $HOME/.ssh/known_hosts
